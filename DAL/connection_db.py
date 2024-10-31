8ee38edccc59fb30eabb1fbd6b299a9f55ce3254
𝑰𝒏̃𝒂𝒌𝒊 𝒁𝒂́𝒓𝒂𝒕𝒆 愛 <zaarate7ignacio@gmail.com>
2024-10-31 04:29:22 -0300

    Conexion a db

---
+++ DAL/connection_db.py
@@ -0,0 +1,10 @@
+from mysql.connector import (connection)
+
+cnx = connection.MySQLConnection(user='root', password='', host='127.0.0.1', database='empresa_test')
+cursor = cnx.cursor()
+cursor.execute("SELECT * FROM employee;")
+for registro in cursor:
+    print(registro)
+cnx.close()
+
+# DESGRACIAU PROBLEMA
