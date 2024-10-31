90602f6b38a7760baf04b9cd3e57ecdfd0a07cd6
𝑰𝒏̃𝒂𝒌𝒊 𝒁𝒂́𝒓𝒂𝒕𝒆 愛 <zaarate7ignacio@gmail.com>
2024-10-31 06:00:04 -0300

    Terminado.

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def del_supplier(cnx):
     cnx.commit()
     print("Proveedor eliminado correctamente.")
 
-# Funciones para sub-menú de 
+# Funciones para sub-menú de Clientes
 def add_client(cnx):
     name = input("Nombre del cliente: ")
     contact = input("Contacto del cliente: ")
@@ -107,7 +107,7 @@ def view_facture(cnx):
     cursor = cnx.cursor
     cursor.execute("SELECT * FROM facture")
     facture = cursor.fetchall()
-    headers = ("ID Factura", "ID Cliente", "TOTAL")
+    headers = ["ID Factura", "ID Cliente", "TOTAL"]
     print(tabulate(facture, headers=headers, tablefmt="pretty"))
 
 def del_facture(cnx):
@@ -127,3 +127,211 @@ def add_product(cnx):
     cursor.execute("INSERT INTO product (name, price, stock, id_inventory) VALUES (?, ?, ?, ?)", (name, price, stock, id_inventory))
     cnx.commit()
     print("Producto agregado correctamente.")
+
+def view_product(cnx):
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM product")
+    product = cursor.fetchall()
+    headers = ["ID Producot", "Nombre", "Precio", "Stock", "ID Inventario"]
+    print(tabulate(product, headers=headers, tablefmt="pretty"))
+
+def del_product(cnx):
+    id_product = input("ID de producto a eliminar: ")
+    cursor = cnx.cursor()
+    cursor.execute("DELETE FROM product WHERE id_product = ?", (id_product))
+    cnx.commit()
+    print("Producto eliminado correctamente.")
+
+# Funciones para sub-menú de Proyecto.
+def add_project(cnx):
+    name = input("Nombre del proyecto: ")
+    budget = float(input("Presupuesto: "))
+    cursor = cnx.cursor()
+    cursor.execute("INSERT INTO project (name, budget) VALUES (?, ?)", (name, budget))
+    cnx.commit()
+    print("Proyecto agregado correctamente")
+
+def view_project(cnx):
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM project")
+    project = cursor.fetchall()
+    headers = ["ID Proyecto", "Nombre", "Presupuesto"]
+    print(tabulate(project, headers=headers, tablefmt="pretty"))
+
+def del_project(cnx):
+    id_project = input("ID de proyecto a eliminar: ")
+    cursor = cnx.cursor()
+    cursor.execute("DELETE FROM project WHERE id_project = ?", (id_project))
+    cnx.commit()
+    print("Proyecto eliminado correctamente.")
+
+
+# Sub-menú por área.
+def menu_employee(cnx):
+    while True:
+        print("\n--- Menú de Empleados ---")
+        print("1. Agregar empleado.")
+        print("2. Ver empleados.")
+        print("3. Eliminar empleado.")
+        print("4. Salir")
+        
+        option = input("Selecciona una opción: ")
+        
+        if option == "1":
+            add_employee()
+        elif option == "2":
+            view_employee()
+        elif option == "3":
+            del_employee()
+        elif option == "4":
+            return
+        else:
+            print("Opción no válida.")
+
+def menu_department(cnx):
+    while True:
+        print("\n--- Menú de Departamentos ---")
+        print("1. Agregar departamento.")
+        print("2. Ver departamentos.")
+        print("3. Eliminar departamento.")
+        print("4. Salir")
+        
+        option = input("Selecciona una opción: ")
+        
+        if option == "1":
+            add_department()
+        elif option == "2":
+            view_deparment()
+        elif option == "3":
+            del_department()
+        elif option == "4":
+            return
+        else:
+            print("Opción no válida.")
+
+def menu_supplier(cnx):
+    while True:
+        print("\n--- Menú de Proveedores ---")
+        print("1. Agregar proveedor.")
+        print("2. Ver proveedores.")
+        print("3. Eliminar proveedor.")
+        print("4. Salir")
+        
+        option = input("Selecciona una opción: ")
+        
+        if option == "1":
+            add_supplier()
+        elif option == "2":
+            view_supplier()
+        elif option == "3":
+            del_supplier()
+        elif option == "4":
+            return
+        else:
+            print("Opción no válida.")
+
+def menu_client(cnx):
+    while True:
+        print("\n--- Menú de Clientes ---")
+        print("1. Agregar cliente.")
+        print("2. Ver clientes.")
+        print("3. Eliminar cliente.")
+        print("4. Salir")
+        
+        option = input("Selecciona una opción: ")
+        
+        if option == "1":
+            add_client()
+        elif option == "2":
+            view_client()
+        elif option == "3":
+            del_client()
+        elif option == "4":
+            return
+        else:
+            print("Opción no válida.")
+
+def menu_facture(cnx):
+    while True:
+        print("\n--- Menú de Facturas ---")
+        print("1. Agregar factura.")
+        print("2. Ver facturas.")
+        print("3. Eliminar factura.")
+        print("4. Salir")
+        
+        option = input("Selecciona una opción: ")
+        
+        if option == "1":
+            add_facture()
+        elif option == "2":
+            view_facture()
+        elif option == "3":
+            del_facture()
+        elif option == "4":
+            return
+        else:
+            print("Opción no válida.")
+
+def menu_product(cnx):
+    while True:
+        print("\n--- Menú de Productos ---")
+        print("1. Agregar producto.")
+        print("2. Ver productos.")
+        print("3. Eliminar producto.")
+        print("4. Salir")
+        
+        option = input("Selecciona una opción: ")
+        
+        if option == "1":
+            add_product()
+        elif option == "2":
+            view_product()
+        elif option == "3":
+            del_product()
+        elif option == "4":
+            return
+        else:
+            print("Opción no válida.")
+
+# Menu general
+def menu_general(cnx):
+    while True:
+        print("\n--- Menú Principal ---")
+        print("1. Menú de Empleados")
+        print("2. Menú de Departamentos")
+        print("3. Menú de Proveedores")
+        print("4. Menú de Clientes")
+        print("5. Menú de Facturas")
+        print("6. Menú de Productos")
+        print("7. Salir")
+        
+        opcion = input("Selecciona una opción: ")
+        
+        if opcion == "1":
+            menu_employee(cnx)
+        elif opcion == "2":
+            menu_department(cnx)
+        elif opcion == "3":
+            menu_supplier(cnx)
+        elif opcion == "4":
+            menu_client(cnx)
+        elif opcion == "5":
+            menu_facture(cnx)
+        elif opcion == "6":
+            menu_product(cnx)
+        elif opcion == "7":
+            print("Saliendo del programa. Guardando datos.")
+            break
+        else:
+            print("Opción no válida. Intente de nuevo.")
+
+# Función principal
+def main():
+    cnx = sqlite3.connect('empresa_test.connection_db')
+    try:
+        menu_general(cnx)
+    finally:
+        cnx.close()
+
+if __name__ == "__main__":
+    main()
